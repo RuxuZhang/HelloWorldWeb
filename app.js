dffabe8a542ba127902059d4a0bad72a146fa327
Yang Hu <whoyoung@gatech.edu>
2019-04-22 14:19:25 -0500

    part2 templat

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var time = require('./routes/time');
+var videosRouter = require('./routes/videos');
 
 var app = express();
 
@@ -22,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/api/time', time);
+app.use('/api/videos', videosRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
